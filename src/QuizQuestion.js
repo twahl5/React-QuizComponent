f0659238af66549a5797d50efbb1ffdb2a358128
twahl5 <twahl@tamu.edu>
2019-04-01 19:58:00 -0400

    button component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import QUizQuestionButton from './QuizQuestionButton.js';
+import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
 	render() {
